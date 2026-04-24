13b619e51bd813b6e4c41f9c1c0001b8342e6853
Ahmadqadri147 <ah50923362@gmail.com>
2026-04-24 12:17:36 +0530

    pre-fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const app = express()
 app.use(cors(
     {
-        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+        origin: true,
         credentials: true
     }
 ))
